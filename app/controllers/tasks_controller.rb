a2289ff33db133715b7544fbafff477e522d7f20
charles noppe <noppe.charles@gmail.com>
2019-05-21 15:25:47 +0200

    ex

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
